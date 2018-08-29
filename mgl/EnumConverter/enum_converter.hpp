0d8a0b957abc79bbf8d2d564c859e0b22aa2d719
Cvelth <Cvelth.mail@gmail.com>
2018-08-29 22:43:51 +0300

    ShaderVariable was implemented. ShaderVariableDataType was added to enum_converter. Major change to enum_converter: now back_convertion is named as mgl::enum_converter::convert_to_##enum_typename.

--- mgl/EnumConverter/enum_converter.hpp
+++ mgl/EnumConverter/enum_converter.hpp
@@ -4,11 +4,12 @@
 	enum class enum_name;\
 	namespace enum_converter {\
 		uint32_t convert(enum_name);\
-		enum_name convert(uint32_t);\
+		enum_name convert_to_##enum_name(uint32_t);\
 	}\
 }
 
 Convertion(ShaderType);
+Convertion(ShaderVariableDataType);
 
 #include "mgl/exceptions.hpp"
 DefineNewMGLException(EnumConvertionError);
--- mgl/ShaderProgram/ShaderVariable.hpp
+++ mgl/ShaderProgram/ShaderVariable.hpp
@@ -1,6 +1,31 @@
 #pragma once
+#include <cstdint>
 namespace mgl {
-	class ShaderVariable {
+	enum class ShaderVariableDataType {
+		Float, Float_2, Float_3, Float_4,
+		Double, Double_2, Double_3, Double_4,
+		Int, Int_2, Int_3, Int_4,
+		UInt, UInt_2, UInt_3, UInt_4,
+		Bool, Bool_2, Bool_3, Bool_4,
+		Float_2x2, Float_3x3, Float_4x4,
+		Float_2x3, Float_2x4, Float_3x2,
+		Float_3x4, Float_4x2, Float_4x3,
+		Double_2x2, Double_3x3, Double_4x4,
+		Double_2x3, Double_2x4, Double_3x2,
+		Double_3x4, Double_4x2, Double_4x3,
+	};
+	enum class ShaderVariableType {
+		Attribute, Uniform
+	};
 
+	struct ShaderVariable {
+		char const*const name;
+		ShaderVariableType const& type;
+		uint32_t const location;
+		ShaderVariableDataType const& data_type;
+
+		explicit ShaderVariable(char const*const name, ShaderVariableType const& type,
+								int location, ShaderVariableDataType const& d_type)
+			: name(name), type(type), location(location), data_type(d_type) {}
 	};
 }
