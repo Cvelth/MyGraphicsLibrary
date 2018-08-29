d63616f5fff9b3f28d50476c5215155aa94dcd29
Cvelth <Cvelth.mail@gmail.com>
2018-08-29 19:12:32 +0300

    Basic lib project was added. Versioning system was implemented. Current version: v2.0.0.121.

---
+++ mgl/lib.cpp
@@ -0,0 +1,10 @@
+#include "version.hpp"
+#include "mgl.hpp"
+
+#include <string>
+#include <sstream>
+char const* mgl::get_version() {
+	std::ostringstream s;
+	s << Version_Major << '.' << Version_Minor << '.' << Version_Patch << '(' << Version_Build << ')';
+	return s.str().c_str();
+}
