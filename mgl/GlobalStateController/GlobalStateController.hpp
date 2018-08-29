ef3cda9793182d3e01bf08f96b4426e5914aadcd
Cvelth <Cvelth.mail@gmail.com>
2018-08-29 23:40:49 +0300

    m_shader_program_used was added to GlobalStateController.

--- mgl/GlobalStateController/GlobalStateController.hpp
+++ mgl/GlobalStateController/GlobalStateController.hpp
@@ -1,9 +1,15 @@
 #pragma once
 namespace mgl {
+	class ShaderProgram;
+
 	class GlobalStateController {
+		static ShaderProgram *m_shader_program_used;
 	public:
 		GlobalStateController() = delete;
 		GlobalStateController(GlobalStateController const& other) = delete;
 		GlobalStateController(GlobalStateController &&other) =delete;
+
+		static void use_shader_program(ShaderProgram *program);
+		static ShaderProgram const* used_shader_program();
 	};
 }
