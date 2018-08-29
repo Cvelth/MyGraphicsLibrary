8848d956364f09e0e4812e53d6f9c91a6c874d0c
Cvelth <Cvelth.mail@gmail.com>
2018-08-30 02:23:14 +0300

    GlobalStateController now controls bound Buffers in addition to used ShaderProgram.

--- mgl/GlobalStateController/GlobalStateController.cpp
+++ mgl/GlobalStateController/GlobalStateController.cpp
@@ -1,14 +1,31 @@
 #include "mgl/dependencies/OpenGL_Dependency/opengl_dependency.hpp"
 #include "GlobalStateController.hpp"
+#include "mgl/EnumConverter/enum_converter.hpp"
 
 #include "mgl/ShaderProgram/ShaderProgram.hpp"
 mgl::ShaderProgram *mgl::GlobalStateController::m_shader_program_used = nullptr;
 void mgl::GlobalStateController::use_shader_program(ShaderProgram *program) {
-	if (m_shader_program_used != program) {
+	if (!program)
+		glUseProgram(0);
+	else if (m_shader_program_used != program)
 		glUseProgram(program->m_id);
-		m_shader_program_used = program;
-	}
+	else
+		return;
+	m_shader_program_used = program;
 }
 mgl::ShaderProgram const* mgl::GlobalStateController::used_shader_program() {
 	return m_shader_program_used;
+}
+
+#include "mgl/VertexData/Buffer.hpp"
+std::pair<mgl::MultiBuffer*, size_t> mgl::GlobalStateController::m_buffers_bound[14];
+void mgl::GlobalStateController::bind_buffer(BufferBindingPoint const& binding_point, MultiBuffer *buffer, size_t index) {
+	if (!buffer)
+		glBindBuffer(mgl::enum_converter::convert(binding_point), 0);
+	else if (!bound_buffer(binding_point).first || buffer->m_ids[index] != bound_buffer(binding_point).first->m_ids[index])
+		glBindBuffer(mgl::enum_converter::convert(binding_point), buffer->m_ids[index]);
+	m_buffers_bound[size_t(binding_point)] = std::make_pair(buffer, index);
+}
+std::pair<mgl::MultiBuffer const*, size_t> const mgl::GlobalStateController::bound_buffer(BufferBindingPoint const& binding_point) {
+	return m_buffers_bound[size_t(binding_point)];
 }
--- mgl/GlobalStateController/GlobalStateController.hpp
+++ mgl/GlobalStateController/GlobalStateController.hpp
@@ -1,9 +1,15 @@
 #pragma once
+#include <utility>
 namespace mgl {
 	class ShaderProgram;
+	class MultiBuffer;
+	enum class BufferBindingPoint;
 
 	class GlobalStateController {
 		static ShaderProgram *m_shader_program_used;
+		static std::pair<MultiBuffer*, size_t> m_buffers_bound[14];
+	protected:
+
 	public:
 		GlobalStateController() = delete;
 		GlobalStateController(GlobalStateController const& other) = delete;
@@ -11,5 +17,8 @@ namespace mgl {
 
 		static void use_shader_program(ShaderProgram *program);
 		static ShaderProgram const* used_shader_program();
+
+		static void bind_buffer(BufferBindingPoint const& binding_point, MultiBuffer *buffer, size_t index = 0);
+		static std::pair<MultiBuffer const*, size_t> const bound_buffer(BufferBindingPoint const& binding_point);
 	};
 }
