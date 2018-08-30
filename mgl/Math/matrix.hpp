09977f532ae27ed5e1e757daedec15ff9f20cb15
Cvelth <Cvelth.mail@gmail.com>
2018-08-31 02:22:13 +0300

    basic_matrix was fully implemented.

--- mgl/Math/matrix.hpp
+++ mgl/Math/matrix.hpp
@@ -5,37 +5,9 @@
 DefineNewMGLException(MatrixIndexOutOfBounds);
 
 namespace mgl::math {
+	enum MatrixValue { ZeroMatrix = 0, IdentityMatrix = 1 };
 	template<typename T, size_t R, size_t C>
-	class matrix_interface {
-		using row_type = basic_vector<T, C>;
-	public:
-		virtual size_t size() const abstract;
-		virtual T const* begin() const abstract;
-		virtual T* begin() abstract;
-		virtual T const* end() const abstract;
-		virtual T* end() abstract;
-
-		virtual row_type const& operator[](size_t r) const abstract;
-		virtual row_type const& row(size_t r) const {
-			return operator[](r);
-		}
-		virtual row_type& operator[](size_t r) abstract;
-		virtual row_type& row(size_t r) {
-			return operator[](r);
-		}
-
-		virtual T const& operator()(size_t r, size_t c) const abstract;
-		T const& at(size_t r, size_t c) const {
-			return operator()(r, c);
-		}
-		virtual T& operator()(size_t r, size_t c) abstract;
-		T& at(size_t r, size_t c) {
-			return operator()(r, c);
-		}
-	};
-
-	template<typename T, size_t R, size_t C>
-	class basic_matrix : public basic_vector<basic_vector<T, C>, R>, public matrix_interface<T, R, C> {
+	class basic_matrix : public basic_vector<basic_vector<T, C>, R> {
 		using row_type = basic_vector<T, C>;
 		using base_type = basic_vector<row_type, R>;
 	protected:
@@ -61,31 +33,32 @@ namespace mgl::math {
 			set_rows(++r, row_type(tail)...);
 		}
 	public:
-		basic_matrix(basic_matrix<T, C, R> const& other) {
-			std::copy(other.begin(), other.end(), data);
+		basic_matrix(MatrixValue mv = IdentityMatrix) : basic_vector<basic_vector<T, C>, R>() {
+			if (mv == IdentityMatrix)
+			for (size_t i = 0; i < std::min(R, C); i++)
+				data[i][i] = T(1);
 		}
-		basic_matrix(basic_matrix<T, C, R>&& other) {
-			std::move(other.begin(), other.end(), data);
-		}
+		basic_matrix(basic_matrix<T, R, C> const& other) : basic_vector<basic_vector<T, C>, R>(other) {}
+		basic_matrix(basic_matrix<T, R, C> &&other) : basic_vector<basic_vector<T, C>, R>(other) {}
 		template <typename... Tail>
-		basic_matrix(typename std::enable_if<sizeof...(Tail) + 1 <= R * C, T>::type const& head = T(0), Tail... tail) {
+		basic_matrix(typename std::enable_if<sizeof...(Tail) + 1 <= R * C, T>::type const& head = T(0), Tail... tail) : basic_vector<basic_vector<T, C>, R>() {
 			set_values(0, 0, head, tail...);
 		}
 		template <typename... Tail>
-		basic_matrix(typename std::enable_if<sizeof...(Tail) + 1 <= R, row_type>::type const& head = row_type(0), Tail... tail) {
+		basic_matrix(typename std::enable_if<sizeof...(Tail) + 1 <= R, row_type>::type const& head = row_type(0), Tail... tail) : basic_vector<basic_vector<T, C>, R>() {
 			set_rows(0, head, tail...);
 		}
-		basic_matrix(std::initializer_list<std::initializer_list<T>> const& list) {
+		basic_matrix(std::initializer_list<std::initializer_list<T>> const& list) : basic_vector<basic_vector<T, C>, R>() {
 			size_t r = 0;
 			for (auto &it : list)
 				data[r++] = row_type(it);
 		}
-		basic_matrix(std::initializer_list<std::initializer_list<T>> &&list) {
+		basic_matrix(std::initializer_list<std::initializer_list<T>> &&list) : basic_vector<basic_vector<T, C>, R>() {
 			size_t r = 0;
 			for (auto &it : list)
 				data[r++] = row_type(it);
 		}
-		basic_matrix(std::initializer_list<T> const& list) {
+		basic_matrix(std::initializer_list<T> const& list) : basic_vector<basic_vector<T, C>, R>() {
 			size_t r = 0, c = 0;
 			for (auto &it : list) {
 				data[r][c] = it;
@@ -95,7 +68,7 @@ namespace mgl::math {
 				}
 			}
 		}
-		basic_matrix(std::initializer_list<T> &&list) {
+		basic_matrix(std::initializer_list<T> &&list) : basic_vector<basic_vector<T, C>, R>() {
 			size_t r = 0, c = 0;
 			for (auto &it : list) {
 				data[r][c] = it;
@@ -107,28 +80,24 @@ namespace mgl::math {
 		}
 
 		template<typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
-		basic_matrix(matrix_interface<T_O, R_O, C_O> const& other, typename std::enable_if<(R_O <= R && C_O <= C), void*>::type less = nullptr) {
+		basic_matrix(basic_matrix<T_O, R_O, C_O> const& other, typename std::enable_if<(R_O <= R && C_O <= C), void*>::type less = nullptr) : basic_vector<basic_vector<T, C>, R>() {
 			for (int r = 0; r < R_O; r++)
-				data[r] = other[r];
-			for (int r = R_O; r < R; r++)
-				data[r].clear();
+				data[r] = basic_vector<T, C>{other[r]};
 		}
 		template<typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
-		basic_matrix(matrix_interface<T_O, R_O, C_O> &&other, typename std::enable_if<(R_O <= R && C_O <= C), void*>::type less = nullptr) {
+		basic_matrix(basic_matrix<T_O, R_O, C_O> &&other, typename std::enable_if<(R_O <= R && C_O <= C), void*>::type less = nullptr) : basic_vector<basic_vector<T, C>, R>() {
 			for (int r = 0; r < R_O; r++)
-				data[r] = other[r];
-			for (int r = R_O; r < R; r++)
-				data[r].clear();
+				data[r] = basic_vector<T, C>{other[r]};
 		}
 		template<typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
-		explicit basic_matrix(matrix_interface<T_O, R_O, C_O> const& other, typename std::enable_if<(R_O > R || C_O > C), void*>::type more = nullptr) {
+		explicit basic_matrix(basic_matrix<T_O, R_O, C_O> const& other, typename std::enable_if<(R_O > R || C_O > C), void*>::type more = nullptr) {
 			for (int r = 0; r < std::min(R, R_O); r++)
-				data[r] = row_type(other[r]);
+				data[r] = row_type(other.row(r));
 		}
 		template<typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
-		explicit basic_matrix(matrix_interface<T_O, R_O, C_O> &&other, typename std::enable_if<(R_O > R || C_O > C), void*>::type more = nullptr) {
+		explicit basic_matrix(basic_matrix<T_O, R_O, C_O> &&other, typename std::enable_if<(R_O > R || C_O > C), void*>::type more = nullptr) {
 			for (int r = 0; r < std::min(R, R_O); r++)
-				data[r] = row_type(other[r]);
+				data[r] = row_type(other.row(r));
 		}
 		basic_matrix<T, C, R> const& operator=(basic_matrix<T, C, R> const& other) {
 			std::copy(other.begin(), other.end(), data);
@@ -139,41 +108,178 @@ namespace mgl::math {
 			return *this;
 		}
 
-		virtual size_t size() const override {
+		size_t size() const {
 			return C * R;
 		}
-		virtual T const* begin() const override {
+		T const* begin() const {
 			return base_type::begin()->begin();
 		}
-		virtual T* begin() override {
+		T* begin() {
 			return base_type::begin()->begin();
 		}
-		virtual T const* end() const override {
+		T const* end() const {
 			return base_type::end()->begin();
 		}
-		virtual T* end() override {
+		T* end() {
 			return base_type::end()->begin();
 		}
 
-		virtual T const& operator()(size_t r, size_t c) const override {
+		T const& at(size_t r, size_t c) const {
 			if (r >= R || c >= C)
 				throw Exceptions::MatrixIndexOutOfBounds();
 			return data[r][c];
 		}
-		virtual T& operator()(size_t r, size_t c) override {
+		T& at(size_t r, size_t c) {
 			if (r >= R || c >= C)
 				throw Exceptions::MatrixIndexOutOfBounds();
 			return data[r][c];
 		}
-		virtual row_type const& operator[](size_t r) const override {
+		T const& operator()(size_t r, size_t c) const {
+			return at(r, c);
+		}
+		T& operator()(size_t r, size_t c) {
+			return at(r, c);
+		}
+		row_type const& operator[](size_t r) const {
 			if (r >= R)
 				throw Exceptions::MatrixIndexOutOfBounds();
 			return base_type::operator[](r);
 		}
-		virtual row_type& operator[](size_t r) override {
+		row_type& operator[](size_t r) {
 			if (r >= R)
 				throw Exceptions::MatrixIndexOutOfBounds();
 			return base_type::operator[](r);
+		}
+		basic_vector<T, C> const& row(size_t r) const {
+			return operator[](r);
+		}
+		basic_vector<T, C>& row(size_t r) {
+			return operator[](r);
+		}
+
+		void fill(T const& value) {
+			for (size_t r = 0; r < R; r++)
+				for (size_t c = 0; c < C; c++)
+					data[r][c] = value;
+		}
+
+		template<typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type, typename = typename std::enable_if<(R_O <= R && C_O <= C)>::type>
+		basic_matrix<T, R, C> const& operator+=(basic_matrix<T_O, R_O, C_O> const& other) {
+			for (size_t r = 0; r < std::min(R, R_O); r++)
+				for (size_t c = 0; c < std::min(C, C_O); c++)
+					data[r][c] += T(other(r, c));
+			return *this;
+		}
+		template<typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type, typename = typename std::enable_if<(R_O <= R && C_O <= C)>::type>
+		basic_matrix<T, R, C> const& operator-=(basic_matrix<T_O, R_O, C_O> const& other) {
+			for (size_t r = 0; r < std::min(R, R_O); r++)
+				for (size_t c = 0; c < std::min(C, C_O); c++)
+					data[r][c] -= T(other(r, c));
+			return *this;
+		}
+		template<typename T_O, size_t R, size_t C, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type, typename = typename std::enable_if<(R == C)>::type>
+		basic_matrix<T, R, C> const& operator*=(basic_matrix<T_O, R, C> const& other) {
+			return (*this = *this * other);
+		}
+		template<typename T_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+		basic_matrix<T, R, C> const& operator*=(T_O const& q) {
+			for (size_t r = 0; r < R; r++)
+				for (size_t c = 0; c < C; c++)
+					data[r][c] *= T(q);
+			return *this;
+		}
+		template<typename T_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+		basic_matrix<T, R, C> const& operator/=(T_O const& q) {
+			for (size_t r = 0; r < R; r++)
+				for (size_t c = 0; c < C; c++)
+					data[r][c] /= T(q);
+			return *this;
+		}
+
+		basic_matrix<T, R, C> const operator-() const {
+			basic_matrix<T, R, C> res;
+			for (size_t i = 0; i < S; i++)
+				res.data[i] = -data[i];
+			return res;
 		}
 	};
+	
+	template<typename T, size_t R, size_t C, typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type, typename = typename std::enable_if<(R_O == R && C_O == C)>::type>
+	bool operator==(basic_matrix<T, R, C> const& v1, basic_matrix<T_O, R_O, C_O> const& v2) {
+		for (size_t i = 0; i < R; i++)
+			if (v1.row(i) != v2.row(i))
+				return false;
+		return true;
+	}
+	template<typename T, size_t R, size_t C, typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type, typename = typename std::enable_if<(R_O == R && C_O == C)>::type>
+	bool operator!=(basic_matrix<T, R, C> const& v1, basic_matrix<T_O, R_O, C_O> const& v2) {
+		return !(v1 == v2);
+	}
+
+	template<typename T, size_t R, size_t C, typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type, typename = typename std::enable_if<(R_O == C)>::type>
+	auto const operator*(basic_matrix<T, R, C> const& v1, basic_matrix<T_O, R_O, C_O> const& v2) {
+		basic_matrix<decltype(v1[0][0] * v2[0][0]), R, C_O> res(ZeroMatrix);
+		for (size_t i = 0; i < R; i++)
+			for (size_t k = 0; k < C_O; k++)
+				for (size_t j = 0; j < C; j++)
+					res[i][k] += v1[i][j] * v2[j][k];
+		return res;
+	}
+
+	template<typename T, size_t R, size_t C, typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+	auto const operator+(basic_matrix<T, R, C> const& v1, basic_matrix<T_O, R_O, C_O> const& v2) {
+		basic_matrix<decltype(v1[0][0] + v2[0][0]), std::max(R, R_O), std::max(C, C_O)> res{v1};
+		return res += v2;
+	}
+	template<typename T, size_t R, size_t C, typename T_O, size_t R_O, size_t C_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+	auto const operator-(basic_matrix<T, R, C> const& v1, basic_matrix<T_O, R_O, C_O> const& v2) {
+		basic_matrix<decltype(v1[0][0] - v2[0][0]), std::max(R, R_O), std::max(C, C_O)> res{v1};
+		return res -= v2;
+	}
+	
+	template<typename T, size_t R, size_t C, typename T_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+	auto const operator*(basic_matrix<T, R, C> const& v, T_O const& q) {
+		basic_matrix<decltype(v[0][0] * q), R, C> res{v};
+		return res *= q;
+	}
+	template<typename T, size_t R, size_t C, typename T_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+	auto const operator*(T_O const& q, basic_matrix<T, R, C> const& v) {
+		basic_matrix<decltype(v[0][0] * q), R, C> res{v};
+		return res *= q;
+	}
+	template<typename T, size_t R, size_t C, typename T_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+	auto const operator/(basic_matrix<T, R, C> const& v, T_O const& q) {
+		basic_matrix<decltype(v[0][0] / q), R, C> res{v};
+		return res /= q;
+	}
+	template<typename T, size_t R, size_t C, typename T_O, typename = typename std::enable_if<std::is_convertible<T_O, T>::value>::type>
+	auto const operator/(T_O const& q, basic_matrix<T, R, C> const& v) {
+		basic_matrix<decltype(q / v[0][0]), R, C> res{v};
+		for (size_t r = 0; r < R; r++)
+			for (size_t c = 0; c < C; c++)
+				data[r][c] = q / data[r][c];
+		return res;
+	}
+
+	class matrix2f : public basic_matrix<float, 2u, 2u> { public: using basic_matrix::basic_matrix; };
+	class matrix3f : public basic_matrix<float, 3u, 3u> { public: using basic_matrix::basic_matrix; };
+	class matrix4f : public basic_matrix<float, 4u, 4u> { public: using basic_matrix::basic_matrix; };
+
+	class matrix2d : public basic_matrix<double, 2u, 2u> { public: using basic_matrix::basic_matrix; };
+	class matrix3d : public basic_matrix<double, 3u, 3u> { public: using basic_matrix::basic_matrix; };
+	class matrix4d : public basic_matrix<double, 4u, 4u> { public: using basic_matrix::basic_matrix; };
+
+	class matrix2b : public basic_matrix<uint8_t, 2u, 2u> { public: using basic_matrix::basic_matrix; };
+	class matrix3b : public basic_matrix<uint8_t, 3u, 3u> { public: using basic_matrix::basic_matrix; };
+	class matrix4b : public basic_matrix<uint8_t, 4u, 4u> { public: using basic_matrix::basic_matrix; };
+
+	class matrix2u : public basic_matrix<uint32_t, 2u, 2u> { public: using basic_matrix::basic_matrix; };
+	class matrix3u : public basic_matrix<uint32_t, 3u, 3u> { public: using basic_matrix::basic_matrix; };
+	class matrix4u : public basic_matrix<uint32_t, 4u, 4u> { public: using basic_matrix::basic_matrix; };
+
+	class matrix2i : public basic_matrix<int32_t, 2u, 2u> { public: using basic_matrix::basic_matrix; };
+	class matrix3i : public basic_matrix<int32_t, 3u, 3u> { public: using basic_matrix::basic_matrix; };
+	class matrix4i : public basic_matrix<int32_t, 4u, 4u> { public: using basic_matrix::basic_matrix; };
+
+	class matrix : public matrix4f { public: using matrix4f::matrix4f; };
 }
