310496ad0e46aeb9c695d17d66e06745506179a1
Cvelth <Cvelth.mail@gmail.com>
2018-08-30 04:33:32 +0300

    GlobalStateController now stores bound vertex_array.

--- mgl/GlobalStateController/GlobalStateController.cpp
+++ mgl/GlobalStateController/GlobalStateController.cpp
@@ -22,10 +22,27 @@ std::pair<mgl::MultiBuffer*, size_t> mgl::GlobalStateController::m_buffers_bound
 void mgl::GlobalStateController::bind_buffer(BufferBindingPoint const& binding_point, MultiBuffer *buffer, size_t index) {
 	if (!buffer)
 		glBindBuffer(mgl::enum_converter::convert(binding_point), 0);
-	else if (!bound_buffer(binding_point).first || buffer->m_ids[index] != bound_buffer(binding_point).first->m_ids[index])
+	else if (!bound_buffer(binding_point).first || buffer->m_ids[index] != bound_buffer(binding_point).first->m_ids[bound_buffer(binding_point).second])
 		glBindBuffer(mgl::enum_converter::convert(binding_point), buffer->m_ids[index]);
+	else
+		return;
 	m_buffers_bound[size_t(binding_point)] = std::make_pair(buffer, index);
 }
 std::pair<mgl::MultiBuffer const*, size_t> const mgl::GlobalStateController::bound_buffer(BufferBindingPoint const& binding_point) {
 	return m_buffers_bound[size_t(binding_point)];
+}
+
+#include "mgl/VertexData/VertexArray.hpp"
+std::pair<mgl::MultiVertexArray*, size_t> mgl::GlobalStateController::m_vertex_array_bound;
+void mgl::GlobalStateController::bind_vertex_array(MultiVertexArray *vertex_array, size_t index) {
+	if (!vertex_array)
+		glBindVertexArray(0);
+	else if (!m_vertex_array_bound.first || vertex_array->m_ids[index] != m_vertex_array_bound.first->m_ids[m_vertex_array_bound.second])
+		glBindVertexArray(vertex_array->m_ids[index]);
+	else
+		return;
+	m_vertex_array_bound = std::make_pair(vertex_array, index);
+}
+std::pair<mgl::MultiVertexArray const*, size_t> const mgl::GlobalStateController::bound_vertex_array() {
+	return m_vertex_array_bound;
 }
