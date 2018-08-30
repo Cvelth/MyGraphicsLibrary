20c210390d5314a93419e5b34618d89f8cf74cf6
Cvelth <Cvelth.mail@gmail.com>
2018-08-30 04:32:15 +0300

    ShaderVariableDataType enum class was split. Additional conversions were implemented. ShaderVariable is now class (with public fields).

---
+++ mgl/ShaderProgram/ShaderVariable.cpp
@@ -0,0 +1,147 @@
+#include "ShaderVariable.hpp"
+#include "mgl/EnumConverter/enum_converter.hpp"
+std::pair<mgl::ShaderVariableNonArrayDataType, mgl::ShaderVariableArrayType> mgl::enum_converter::convert_variable_type(ShaderVariableDataType const& v) {
+	switch (v) {
+		case ShaderVariableDataType::Float:   return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_1);
+		case ShaderVariableDataType::Float_2: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_2);
+		case ShaderVariableDataType::Float_3: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_3);
+		case ShaderVariableDataType::Float_4: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_4);
+
+		case ShaderVariableDataType::Double:   return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_1);
+		case ShaderVariableDataType::Double_2: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_2);
+		case ShaderVariableDataType::Double_3: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_3);
+		case ShaderVariableDataType::Double_4: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_4);
+
+		case ShaderVariableDataType::Int:   return std::make_pair(ShaderVariableNonArrayDataType::Int, ShaderVariableArrayType::_1);
+		case ShaderVariableDataType::Int_2: return std::make_pair(ShaderVariableNonArrayDataType::Int, ShaderVariableArrayType::_2);
+		case ShaderVariableDataType::Int_3: return std::make_pair(ShaderVariableNonArrayDataType::Int, ShaderVariableArrayType::_3);
+		case ShaderVariableDataType::Int_4: return std::make_pair(ShaderVariableNonArrayDataType::Int, ShaderVariableArrayType::_4);
+
+		case ShaderVariableDataType::UInt:   return std::make_pair(ShaderVariableNonArrayDataType::UInt, ShaderVariableArrayType::_1);
+		case ShaderVariableDataType::UInt_2: return std::make_pair(ShaderVariableNonArrayDataType::UInt, ShaderVariableArrayType::_2);
+		case ShaderVariableDataType::UInt_3: return std::make_pair(ShaderVariableNonArrayDataType::UInt, ShaderVariableArrayType::_3);
+		case ShaderVariableDataType::UInt_4: return std::make_pair(ShaderVariableNonArrayDataType::UInt, ShaderVariableArrayType::_4);
+
+		case ShaderVariableDataType::Bool:   return std::make_pair(ShaderVariableNonArrayDataType::Bool, ShaderVariableArrayType::_1);
+		case ShaderVariableDataType::Bool_2: return std::make_pair(ShaderVariableNonArrayDataType::Bool, ShaderVariableArrayType::_2);
+		case ShaderVariableDataType::Bool_3: return std::make_pair(ShaderVariableNonArrayDataType::Bool, ShaderVariableArrayType::_3);
+		case ShaderVariableDataType::Bool_4: return std::make_pair(ShaderVariableNonArrayDataType::Bool, ShaderVariableArrayType::_4);
+
+		case ShaderVariableDataType::Float_2x2: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_2x2);
+		case ShaderVariableDataType::Float_3x3: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_3x3);
+		case ShaderVariableDataType::Float_4x4: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_4x4);
+
+		case ShaderVariableDataType::Float_2x3: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_2x3);
+		case ShaderVariableDataType::Float_3x2: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_3x2);
+		case ShaderVariableDataType::Float_4x2: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_4x2);
+
+		case ShaderVariableDataType::Float_2x4: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_2x4);
+		case ShaderVariableDataType::Float_3x4: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_3x4);
+		case ShaderVariableDataType::Float_4x3: return std::make_pair(ShaderVariableNonArrayDataType::Float, ShaderVariableArrayType::_4x3);
+
+		case ShaderVariableDataType::Double_2x2: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_2x2);
+		case ShaderVariableDataType::Double_3x3: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_3x3);
+		case ShaderVariableDataType::Double_4x4: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_4x4);
+																									   
+		case ShaderVariableDataType::Double_2x3: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_2x3);
+		case ShaderVariableDataType::Double_3x2: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_3x2);
+		case ShaderVariableDataType::Double_4x2: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_4x2);
+																									   
+		case ShaderVariableDataType::Double_2x4: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_2x4);
+		case ShaderVariableDataType::Double_3x4: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_3x4);
+		case ShaderVariableDataType::Double_4x3: return std::make_pair(ShaderVariableNonArrayDataType::Double, ShaderVariableArrayType::_4x3);
+		default: throw Exceptions::EnumConvertionError();
+	}
+}
+mgl::ShaderVariableDataType mgl::enum_converter::convert_variable_type(std::pair<ShaderVariableNonArrayDataType, ShaderVariableArrayType> const& v) {
+	switch (v.first) {
+		case ShaderVariableNonArrayDataType::Float:
+			switch (v.second) {
+				case ShaderVariableArrayType::_1: return ShaderVariableDataType::Float;
+				case ShaderVariableArrayType::_2: return ShaderVariableDataType::Float_2;
+				case ShaderVariableArrayType::_3: return ShaderVariableDataType::Float_3;
+				case ShaderVariableArrayType::_4: return ShaderVariableDataType::Float_4;
+				case ShaderVariableArrayType::_2x2:	return ShaderVariableDataType::Float_2x2;
+				case ShaderVariableArrayType::_3x2:	return ShaderVariableDataType::Float_3x3;
+				case ShaderVariableArrayType::_4x2:	return ShaderVariableDataType::Float_4x4;
+				case ShaderVariableArrayType::_2x3:	return ShaderVariableDataType::Float_2x3;
+				case ShaderVariableArrayType::_3x3:	return ShaderVariableDataType::Float_3x2;
+				case ShaderVariableArrayType::_4x3:	return ShaderVariableDataType::Float_4x2;
+				case ShaderVariableArrayType::_2x4:	return ShaderVariableDataType::Float_2x4;
+				case ShaderVariableArrayType::_3x4:	return ShaderVariableDataType::Float_3x4;
+				case ShaderVariableArrayType::_4x4:	return ShaderVariableDataType::Float_4x3;
+				default: throw Exceptions::EnumConvertionError();
+			}
+		case ShaderVariableNonArrayDataType::Double:
+			switch (v.second) {
+				case ShaderVariableArrayType::_1: return ShaderVariableDataType::Double;
+				case ShaderVariableArrayType::_2: return ShaderVariableDataType::Double_2;
+				case ShaderVariableArrayType::_3: return ShaderVariableDataType::Double_3;
+				case ShaderVariableArrayType::_4: return ShaderVariableDataType::Double_4;
+				case ShaderVariableArrayType::_2x2:	return ShaderVariableDataType::Double_2x2;
+				case ShaderVariableArrayType::_3x2:	return ShaderVariableDataType::Double_3x3;
+				case ShaderVariableArrayType::_4x2:	return ShaderVariableDataType::Double_4x4;
+				case ShaderVariableArrayType::_2x3:	return ShaderVariableDataType::Double_2x3;
+				case ShaderVariableArrayType::_3x3:	return ShaderVariableDataType::Double_3x2;
+				case ShaderVariableArrayType::_4x3:	return ShaderVariableDataType::Double_4x2;
+				case ShaderVariableArrayType::_2x4:	return ShaderVariableDataType::Double_2x4;
+				case ShaderVariableArrayType::_3x4:	return ShaderVariableDataType::Double_3x4;
+				case ShaderVariableArrayType::_4x4:	return ShaderVariableDataType::Double_4x3;
+				default: throw Exceptions::EnumConvertionError();
+			}
+		case ShaderVariableNonArrayDataType::Int:
+			switch (v.second) {
+				case ShaderVariableArrayType::_1: return ShaderVariableDataType::Int;
+				case ShaderVariableArrayType::_2: return ShaderVariableDataType::Int_2;
+				case ShaderVariableArrayType::_3: return ShaderVariableDataType::Int_3;
+				case ShaderVariableArrayType::_4: return ShaderVariableDataType::Int_4;
+				default: throw Exceptions::EnumConvertionError();
+			}
+		case ShaderVariableNonArrayDataType::UInt:
+			switch (v.second) {
+				case ShaderVariableArrayType::_1: return ShaderVariableDataType::UInt;
+				case ShaderVariableArrayType::_2: return ShaderVariableDataType::UInt_2;
+				case ShaderVariableArrayType::_3: return ShaderVariableDataType::UInt_3;
+				case ShaderVariableArrayType::_4: return ShaderVariableDataType::UInt_4;
+				default: throw Exceptions::EnumConvertionError();
+			}
+		case ShaderVariableNonArrayDataType::Bool:
+			switch (v.second) {
+				case ShaderVariableArrayType::_1: return ShaderVariableDataType::Bool;
+				case ShaderVariableArrayType::_2: return ShaderVariableDataType::Bool_2;
+				case ShaderVariableArrayType::_3: return ShaderVariableDataType::Bool_3;
+				case ShaderVariableArrayType::_4: return ShaderVariableDataType::Bool_4;
+				default: throw Exceptions::EnumConvertionError();
+			}
+		default: throw Exceptions::EnumConvertionError();
+	}
+}
+
+size_t mgl::enum_converter::convert_to_number(ShaderVariableArrayType const& v) {
+	switch (v) {
+		case ShaderVariableArrayType::_1: return 1;
+		case ShaderVariableArrayType::_2: return 2;
+		case ShaderVariableArrayType::_3: return 3;
+		case ShaderVariableArrayType::_4: return 4;
+		case ShaderVariableArrayType::_2x2:	return 4;
+		case ShaderVariableArrayType::_3x2:	return 6;
+		case ShaderVariableArrayType::_4x2:	return 8;
+		case ShaderVariableArrayType::_2x3:	return 6;
+		case ShaderVariableArrayType::_3x3:	return 9;
+		case ShaderVariableArrayType::_4x3:	return 12;
+		case ShaderVariableArrayType::_2x4:	return 8;
+		case ShaderVariableArrayType::_3x4:	return 12;
+		case ShaderVariableArrayType::_4x4:	return 16;
+		default: throw Exceptions::EnumConvertionError();
+	}
+}
+size_t mgl::enum_converter::get_size(ShaderVariableNonArrayDataType const& v) {
+	switch (v) {
+		case ShaderVariableNonArrayDataType::Float: return sizeof(float);
+		case ShaderVariableNonArrayDataType::Double: return sizeof(double);
+		case ShaderVariableNonArrayDataType::Int: return sizeof(int);
+		case ShaderVariableNonArrayDataType::UInt: return sizeof(unsigned int);
+		case ShaderVariableNonArrayDataType::Bool: return sizeof(bool);
+		default: throw Exceptions::EnumConvertionError();
+	}
+}
--- mgl/ShaderProgram/ShaderVariable.hpp
+++ mgl/ShaderProgram/ShaderVariable.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <cstdint>
+#include <utility>
+#include <string>
 namespace mgl {
+	enum class ShaderVariableType {
+		Attribute, Uniform
+	};
 	enum class ShaderVariableDataType {
 		Float, Float_2, Float_3, Float_4,
 		Double, Double_2, Double_3, Double_4,
@@ -12,13 +17,20 @@ namespace mgl {
 		Float_3x4, Float_4x2, Float_4x3,
 		Double_2x2, Double_3x3, Double_4x4,
 		Double_2x3, Double_2x4, Double_3x2,
-		Double_3x4, Double_4x2, Double_4x3,
+		Double_3x4, Double_4x2, Double_4x3
 	};
-	enum class ShaderVariableType {
-		Attribute, Uniform
+	enum class ShaderVariableNonArrayDataType {
+		Float, Double, Int, UInt, Bool
+	};
+	enum class ShaderVariableArrayType {
+		_1, _2, _3, _4,
+		_2x2, _3x2, _4x2,
+		_2x3, _3x3, _4x3,
+		_2x4, _3x4, _4x4
 	};
 
-	struct ShaderVariable {
+	class ShaderVariable {
+	public:
 		std::string const name;
 		ShaderVariableType const type;
 		uint32_t const location;
@@ -28,4 +40,11 @@ namespace mgl {
 								int location, ShaderVariableDataType const& d_type)
 			: name(name), type(type), location(location), data_type(d_type) {}
 	};
+
+	namespace enum_converter {
+		std::pair<ShaderVariableNonArrayDataType, ShaderVariableArrayType> convert_variable_type(ShaderVariableDataType const& v);
+		ShaderVariableDataType convert_variable_type(std::pair<ShaderVariableNonArrayDataType, ShaderVariableArrayType> const& v);
+		size_t convert_to_number(ShaderVariableArrayType const& v);
+		size_t get_size(ShaderVariableNonArrayDataType const& v);
+	}
 }
