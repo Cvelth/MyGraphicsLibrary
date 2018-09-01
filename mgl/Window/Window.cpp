4e97986088679e386ef058a65529a1f1654d0196
Cvelth <Cvelth.mail@gmail.com>
2018-09-01 14:02:13 +0300

    Structure of Window class was implemented.

--- mgl/Window/Window.cpp
+++ mgl/Window/Window.cpp
@@ -1,3 +1,71 @@
+#include "mgl/dependencies/OpenGL_Dependency/opengl_dependency.hpp"
+#include "mgl/dependencies/WindowLibrary_Dependency/window_dependency.hpp"
 #include "Window.hpp"
-mgl::Window::Window() {}
-mgl::Window::~Window() {}
+#include <string>
+mgl::AbstractWindow::AbstractWindow(char const* title, int width, int height, bool is_fullscreen) {
+	if (!glfwInit())
+		throw Exceptions::WindowCreationError();
+
+	m_window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (!m_window) {
+		glfwTerminate();
+		throw Exceptions::WindowCreationError();
+	}
+	if (is_fullscreen) {
+		auto monitor = glfwGetPrimaryMonitor();
+		glfwSetWindowMonitor(m_window, monitor, 0, 0, width, height, glfwGetVideoMode(monitor)->refreshRate);
+	}
+
+	glfwMakeContextCurrent(m_window);
+	glfwSwapInterval(0);
+
+	auto error = glewInit();
+	if (error != GLEW_OK) {
+		glfwDestroyWindow(m_window);
+		glfwTerminate();
+		throw Exceptions::WindowCreationError((std::string("GLEW initialization error: ") +
+											  (char const*) (glewGetErrorString(error))).c_str());
+	}
+}
+mgl::AbstractWindow::~AbstractWindow() {
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
+}
+
+void mgl::AbstractWindow::update() {
+	glfwSwapBuffers(m_window);
+}
+void mgl::AbstractWindow::pollEvents() const {
+	glfwPollEvents();
+}
+void mgl::AbstractWindow::waitEvents() const {
+	glfwWaitEvents();
+}
+void mgl::AbstractWindow::clearWindow() {
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+void mgl::AbstractWindow::setClearColor(mgl::Color const& color) {
+	//to be implemented.
+}
+void mgl::AbstractWindow::setClearColor(const float r, const float g, const float b, const float a) {
+	glClearColor(r, g, b, a);
+}
+
+int mgl::AbstractWindow::width() const {
+	int ret;
+	glfwGetFramebufferSize(m_window, &ret, nullptr);
+	return ret;
+}
+int mgl::AbstractWindow::height() const {
+	int ret;
+	glfwGetFramebufferSize(m_window, nullptr, &ret);
+	return ret;
+}
+std::pair<int, int> mgl::AbstractWindow::size() const {
+	std::pair<int, int> ret;
+	glfwGetFramebufferSize(m_window, &ret.first, &ret.second);
+	return ret;
+}
+bool mgl::AbstractWindow::should_close() const {
+	return glfwWindowShouldClose(m_window);
+}
--- mgl/Window/Window.hpp
+++ mgl/Window/Window.hpp
@@ -1,8 +1,52 @@
 #pragma once
+#include <memory>
+
+struct GLFWwindow;
+
 namespace mgl {
-	class Window {
+	namespace math {
+		class transformation;
+	}
+	class Color;
+
+	class AbstractWindow {
+	private:
+		GLFWwindow *m_window;
 	public:
-		Window();
-		virtual ~Window();
+		explicit AbstractWindow(char const* title, int width, int height, bool is_fullscreen = false);
+		virtual ~AbstractWindow();
+
+		void update();
+		void pollEvents() const;
+		void waitEvents() const;
+		void clearWindow();
+		void setClearColor(Color const& color);
+		void setClearColor(const float r = 0.f, const float g = 0.f,
+						   const float b = 0.f, const float a = 1.f);
+
+		int width() const;
+		int height() const;
+		std::pair<int, int> size() const;
+
+		bool should_close() const;
+	};
+
+	class Window : public AbstractWindow {
+	protected:
+		virtual void initializeRendering() {}
+		virtual void render() {}
+		virtual void clearRendering() {}
+	public:
+		using AbstractWindow::AbstractWindow;
+		void loop() {
+			initializeRendering();
+			while (!should_close()) {
+				render();
+			}
+			clearRendering();
+		}
 	};
 }
+
+#include "mgl/exceptions.hpp"
+DefineNewMGLException(WindowCreationError);
