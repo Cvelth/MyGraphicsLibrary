a9a225d36abb3d13361ad6bf43aad4f8f48b4e26
cvelth <cvelth.mail@gmail.com>
2018-10-11 21:49:50 +0300

    mgl::math was removed. mml is now used instead.

--- mgl/version.hpp
+++ mgl/version.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #define Version_Major 2
 #define Version_Minor 0
-#define Version_Patch 2
-#define Version_Build 197
+#define Version_Patch 3
+#define Version_Build 198
 
 namespace mgl {
 	char const* get_version();
